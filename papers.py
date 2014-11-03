0fc1dd23689385a32d220986f018360fdc68cf91
hebe889900 <jackyan63@gmail.com>
2014-11-03 17:38:22 -0500

    Add the rules for the entry one by one

--- papers.py
+++ papers.py
@@ -38,13 +38,76 @@ def decide(input_file, watchlist_file, countries_file):
         file_contents_countries = file_reader_countries.read()
     json_contents_countries = json.loads(file_contents_countries)
 
-    if set(["passport",
-            "first_name",
-            "last_name",
-            "birth_date",
-            "home",
-            "entry_reason"]).issubset(file_contents_input)is False:
-        return ["Reject"]
+    # If the required information for an entry record is incomplete, the traveler must be rejected.
+    for entry_dictionary in json_contents_input:
+
+        if set(["passport","first_name","last_name","birth_date","home","from","entry_reason"]).issubset(entry_dictionary)is False:
+                return ["Reject"]
+
+            # Assign the string to each key.
+        key_passport = "passport"
+        key_first_name = "first_name"
+        key_last_name = "last name"
+        key_birth_date = "birth_date"
+        key_home = "home"
+        key_from = "from"
+        key_entry_reason = "entry_reason"
+        key_via = "via"
+        key_visa = "visa"
+        key_visa_date = "date"
+        key_country = "country"
+        key_medical_advisory = "medical_advisory"
+        key_visitor_visa_required = "visitor_visa_required"
+        key_transit_visa_required = "transit_visa_required"
+
+
+    #A traveller who is returning and home country is ”KAN” will be accepted, unless some other condition holds.
+
+        if key_home in file_contents_input:
+            if key_entry_reason in file_contents_input:
+                if "kan" in entry_dictionary[key_home]:
+                    if "returning" in entry_dictionary[key_entry_reason]:
+                        return ["Accept"]
+
+    #If the traveller has a name or passport on the watch list, she or he must be sent to secondary processing.
+        for watchlist_dictionary in file_contents_watchlist:
+            if entry_dictionary[key_passport] in watchlist_dictionary:
+                return ["Secondary"]
+
+            if entry_dictionary[key_last_name] in watchlist_dictionary:
+                if entry_dictionary[key_first_name] in watchlist_dictionary:
+                    return ["Secondary"]
+
+    #If the traveler is coming from or via a country that has a medical advisory, he or she must be send to quarantine.
+        for countries_dictionary in file_contents_countries:
+            if countries_dictionary[entry_dictionary[key_from][key_country]][key_medical_advisory].isspace is False:
+                return ["Quarantine"]
+
+            if key_via in entry_dictionary:
+                if countries_dictionary[entry_dictionary[key_via][key_country]][key_medical_advisory].isspace is False:
+                    return ["Quarantine"]
+
+    #If the reason for entry is to visit and the visitor has a passport from a country from which a visitor visa is required,
+    # the traveller must have a valid visa. A valid visa is one that is less than two years old.
+                if countries_dictionary[entry_dictionary[key_via][key_country]][key_visitor_visa_required] == 1:
+                    if key_visa in entry_dictionary:
+                        if entry_dictionary[key_visa][key_visa_date].date().day- datetime.datetime.now().day > 2*365:
+                            return ["Reject"]
+
+    #If the reason for entry is transit and the visitor has a passport from a country from which a transit visa is required,
+    # the traveller must have a valid visa. A valid visa is one that is less than two years old.
+                if countries_dictionary[entry_dictionary[key_via][key_country]][key_transit_visa_required] == 1:
+                    if key_visa in entry_dictionary:
+                        if entry_dictionary[key_visa][key_visa_date].date().day- datetime.datetime.now().day > 2*365:
+                            return ["Reject"]
+
+    #An entry should not be rejected if there is a mismatch between uppercase and lowercase. For example, the case of the country code and passport numbers should not matter.
+
+    #If the reason for entry is returning home and the traveller’s home country is Kanadia (country code: KAN), the traveller will be accepted.
+
+
+
+
 
 
 def valid_passport_format(passport_number):
